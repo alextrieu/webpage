fa0aa1a8bcb085adb6aec6d67e7a44dfcfcb6358
Alex <alextrieu52@gmail.com>
2022-04-04 20:47:38 -0700

    added fetch API

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,27 @@
-console.log('Hello, World!');
+
+// const redText = document.getElementById('red-header').style.color = "red";
+// const blueText = document.getElementById('blue-header').style.color = "blue";
+// const pinkText = document.getElementById('pink-header').style.color = "pink";
+// const greenText = document.getElementById('green-header').style.color = "green";
 
 
-// .headers > h1 
-// store in array
-// for each word in the array, assign it a random color 
+// retrieve the child elements from #header1  
+// let header = document.getElementById("header1").querySelectorAll("span");
 
 
-const redText = document.getElementById('red-header').style.color = "red";
-const blueText = document.getElementById('blue-header').style.color = "blue";
-const pinkText = document.getElementById('pink-header').style.color = "pink";
-const greenText = document.getElementById('green-header').style.color = "green";
+const redText = document.getElementById('red-header');
+const blueText = document.getElementById('blue-header');
+const pinkText = document.getElementById('pink-header');
+const greenText = document.getElementById('green-header');
+const colors = ["red", "blue", "pink", "green"];
 
+const headerBlocks = [redText, blueText, pinkText, greenText];
+
+for (let i = 0; i < headerBlocks.length; i++) {
+  for (let i = 0; i < colors.length; i++) {
+    headerBlocks[i].style.color = colors[i];
+  }
+}
 
 const button = document.getElementById("introduction-header-btn");
 button.addEventListener("mouseover", function( event ) {
@@ -20,4 +31,33 @@ button.addEventListener("mouseover", function( event ) {
     }, 400);
   }, false);
 
-  
+
+// Dialog
+
+const dialog = document.querySelector('.dialog');
+const navbar = document.querySelector('.fa-bars');
+const exitDialog = document.querySelector('.exit-btn');
+
+navbar.addEventListener('click', function() {
+  dialog.style.visibility = "visible";
+});
+
+exitDialog.addEventListener("click", function() {
+    dialog.style.visibility = "hidden";
+});
+
+
+// Promise API
+
+const fetchAPI = fetch('https://catfact.ninja/fact');
+const catFacts = document.getElementById('cat-facts')
+fetchAPI
+  .then( response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then( json => {
+    catFacts.innerText = json.fact;
+  });
